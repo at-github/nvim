92cbb59a15980be89eca88a2b9bdf033e6bfc311
Tarik Amar <tarik.amar@gmail.com>
2022-09-01 00:43:43 +0200

    COC - Change color highlight, visible now

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -3,7 +3,6 @@ local map = vim.api.nvim_set_keymap
 noremap = {noremap = true}
 
 -- Telescope
--- non breaking space in prompt
 map('n', '<leader>ff', ":Telescope find_files prompt_prefix= 🔍  find_command=rg,--ignore,--hidden,--files <cr>", noremap)
 map('n', '<leader>fg', ":Telescope git_files  prompt_prefix=  <cr>", noremap)
 map('n', '<leader>ft', ":Telescope help_tags  prompt_prefix=  <cr>",  noremap)
