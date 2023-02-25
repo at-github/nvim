f8adb162a85593d1edccc9f5c632bffe0702d801
Tarik Amar <tarik.amar@gmail.com>
2023-02-25 10:12:59 +0000

    Vue.js - configure volar lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -35,8 +35,7 @@ end
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "😒", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰋇 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
@@ -151,6 +150,12 @@ lspconfig["sqlls"].setup({
 
 -- configure yamlls
 lspconfig["yamlls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure volar
+lspconfig["volar"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
